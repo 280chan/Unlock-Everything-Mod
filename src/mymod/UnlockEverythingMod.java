5b1975f154c0707bc83a2a1b3fa9a8b5be7afd09
280chan <42588049+280chan@users.noreply.github.com>
2020-07-02 18:45:11 -0700

    version up 4.4.0

--- src/mymod/UnlockEverythingMod.java
+++ src/mymod/UnlockEverythingMod.java
@@ -1,27 +1,31 @@
 package mymod;
 
 import java.util.ArrayList;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer.PlayerClass;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.CardCrawlGame.GameMode;
 import com.megacrit.cardcrawl.helpers.Prefs;
 import com.megacrit.cardcrawl.helpers.SaveHelper;
+import com.megacrit.cardcrawl.relics.AbstractRelic;
 import com.megacrit.cardcrawl.unlock.UnlockTracker;
 
 import basemod.BaseMod;
 import basemod.interfaces.PostInitializeSubscriber;
-import basemod.interfaces.PostUpdateSubscriber;
 
 /**
  * @author 彼君不触
- * @version 6/1/2020
+ * @version 7/2/2020
  * @since 8/26/2018
  */
 
 @SpireInitializer
-public class UnlockEverythingMod implements PostUpdateSubscriber, PostInitializeSubscriber {
-	public static boolean unlocked = false;
+public class UnlockEverythingMod implements PostInitializeSubscriber {
+	public static final Logger LOGGER = LogManager.getLogger(UnlockEverythingMod.class.getName());
 	
 	public static void initialize() {
 		BaseMod.subscribe(new UnlockEverythingMod());
@@ -29,22 +33,20 @@ public class UnlockEverythingMod implements PostUpdateSubscriber, PostInitialize
 
 	@Override
 	public void receivePostInitialize() {
-	}
-
-	public static void unlockAll(){
-		if (CardCrawlGame.mode != GameMode.CHAR_SELECT || unlocked)
-			return;
-		unlocked = true;
 		unlockAscensionLevel(20);
 		unlockFinalAct();
 		unlockDaily();
+		unlockBetaArtAndEnding();
 		unlockRelics();
 		unlockCards();
 	}
-
-	@Override
-	public void receivePostUpdate() {
-		unlockAll();
+	
+	public static void unlockBetaArtAndEnding() {
+		String key = "THE_ENDING";
+		if (!UnlockTracker.achievementPref.getBoolean(key, false)) {
+			UnlockTracker.achievementPref.putBoolean(key, true);
+			UnlockTracker.achievementPref.flush();
+		}
 	}
 	
 	public static void unlockFinalAct() {
@@ -69,36 +71,45 @@ public class UnlockEverythingMod implements PostUpdateSubscriber, PostInitialize
 	
 	public static void unlockCards() {
 		CardLibrary.initialize();
-		for (String id : CardLibrary.CARD_IDS)
-			UnlockTracker.unlockCard(id);
+		for (String id : CardLibrary.CARD_IDS) {
+			UnlockTracker.unlockPref.putInteger(id, 2);
+			UnlockTracker.lockedCards.remove(id);
+			AbstractCard c = com.megacrit.cardcrawl.helpers.CardLibrary.getCard(id);
+			if (c != null && !c.isSeen) {
+				c.isSeen = true;
+				c.unlock();
+				UnlockTracker.seenPref.putInteger(id, 1);
+			}
+		}
+		UnlockTracker.seenPref.flush();
+		UnlockTracker.unlockPref.flush();
 	}
 	
 	public static void unlockRelics() {
 		RelicLibrary.initialize();
 		for (String id : RelicLibrary.RELIC_IDS) {
 			UnlockTracker.unlockPref.putInteger(id, 2);
-			if (com.megacrit.cardcrawl.helpers.RelicLibrary.getRelic(id) != null
-					&& !com.megacrit.cardcrawl.helpers.RelicLibrary.getRelic(id).isSeen) {
-				com.megacrit.cardcrawl.helpers.RelicLibrary.getRelic(id).isSeen = true;
+			UnlockTracker.lockedRelics.remove(id);
+			AbstractRelic r = com.megacrit.cardcrawl.helpers.RelicLibrary.getRelic(id);
+			if (r != null && !r.isSeen) {
+				r.isSeen = true;
 				UnlockTracker.relicSeenPref.putInteger(id, 1);
 			}
 		}
 		UnlockTracker.unlockPref.flush();
 		UnlockTracker.relicSeenPref.flush();
-		UnlockTracker.lockedRelics.clear();
 	}
 	
 	public static void unlockAscensionLevel(int level) {
 		UnlockTracker.hardUnlockOverride("The Silent");
 		UnlockTracker.hardUnlockOverride("Defect");
 		UnlockTracker.hardUnlockOverride("Watcher");
-		ArrayList<Prefs> prefs = new ArrayList<Prefs>();
-		prefs.add(SaveHelper.getPrefs("STSDataVagabond"));
-		prefs.add(SaveHelper.getPrefs("STSDataTheSilent"));
-		prefs.add(SaveHelper.getPrefs("STSDataDefect"));
-		prefs.add(SaveHelper.getPrefs("STSDataWatcher"));
+		ArrayList<Prefs> p = CardCrawlGame.characterManager.getAllPrefs();
+		while (p.size() > 4) {
+			p.remove(4);
+		}
 		
-		for (Prefs pref : prefs) {
+		for (Prefs pref : p) {
 			if (pref.getInteger("WIN_COUNT", 0) == 0)
 				pref.putInteger("WIN_COUNT", 1);
 			if (level < 1)
@@ -108,7 +119,7 @@ public class UnlockEverythingMod implements PostUpdateSubscriber, PostInitialize
 			}
 			pref.putInteger("ASCENSION_LEVEL", level);
 	        pref.putInteger("LAST_ASCENSION_LEVEL", level);
-			BaseMod.logger.info("ASCENSION LEVEL IS NOW: " + level);
+			LOGGER.info("ASCENSION LEVEL IS NOW: " + level);
 			pref.flush();
 		}
 	}
